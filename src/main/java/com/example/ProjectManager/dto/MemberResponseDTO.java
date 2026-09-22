a26cf0ac5ffba90e36de1c27e51bd8887829598a
jusanghyeok <jsmh991102@gmail.com>
2026-09-22 17:15:01 +0900

    refactor: clean up member response dto

--- src/main/java/com/example/ProjectManager/dto/MemberResponseDTO.java
+++ src/main/java/com/example/ProjectManager/dto/MemberResponseDTO.java
@@ -3,11 +3,11 @@ package com.example.ProjectManager.dto;
 import com.example.ProjectManager.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+@Getter // Getter 메서드를 자동으로 생성한다.
+@AllArgsConstructor // 모든 필드를 받는 생성자를 자동으로 생성한다.
 public class MemberResponseDTO {
+
     // 회원의 DB ID
     private Long id;
 
@@ -19,6 +19,4 @@ public class MemberResponseDTO {
 
     // 회원 권한
     private Role role;
-
-
 }
